8c0dd6cbb83e31b3c8fdf92b8c1170a1564e67a9
Massimo Bruni <massimo.bruni@protonmail.com>
2023-06-26 18:21:16 +0200

    Corrected Dashboard Runned Jupyter to write livrables

--- 6_dashboard.py
+++ 6_dashboard.py
@@ -7,6 +7,7 @@ import json
 import requests
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 from outils_general_810 import list_files_in_folder
 from outils_feature_engineering_810 import get_cat_for_obs
@@ -16,10 +17,12 @@ SAMPLE_DATA_PATH = './st_content/sample_data.csv'
 PREPROCESSED_DATA_PATH = './st_content/processed_new_data.csv'
 ORIGINAL_DATA_PATH = './st_content/original_data.csv'
 CUSTOM_CSS = './st_content/style.css'
+HEROKU_APP_PREDICT_PROBA = "https://oc-ds-p7.herokuapp.com/predict_proba"
 HEROKU_APP_PREDICTION_URI = "https://oc-ds-p7.herokuapp.com/solvability_prediction"
 HEROKU_APP_SHAP_FORCE_URI = "https://oc-ds-p7.herokuapp.com/get_shap_force"
 HEROKU_APP_PREPROCESSING_URI = "https://oc-ds-p7.herokuapp.com/preprocess_data"
 TOP_FEATURES_FILES = "./st_content/top_features_profiles/"
+WARNING_MESSAGE = "<span class='warning_mess'>Attention : Rappelez-vous que meme lorsque notre modele est confiant, il peut faire des erreurs. Etudiez-les informations ci-apres pour vous faire votre avis.</span>"
 
 
 def local_css(file_name):
@@ -31,6 +34,12 @@ def local_css(file_name):
 # ------------------------------------------------------------------------------------
 # FONCTIONS D'APPELS D'API
 # ---------------------------------------------------------------------------------
+
+def get_predict_proba(lst_columns, associated_data):
+    dict_customer = {k: v for k, v in zip(lst_columns, associated_data)}
+    input_json = json.dumps(dict_customer)
+    response = requests.post(HEROKU_APP_PREDICT_PROBA, data=input_json)
+    return response.text
 
 
 def predict_with_heroku_app(lst_columns, associated_data):
@@ -89,27 +98,52 @@ def get_rad_val(application, ref_folder=TOP_FEATURES_FILES):
     return dict_radar_values
 
 
+def create_html_list(texts_list):
+    liste_html = "<ul>"
+    for element in texts_list :
+        liste_html = liste_html+f"<li>{element}</li>"
+    liste_html = liste_html+"</ul>"
+    return liste_html
+
+
 # ---------------------------------------------------------------------------------
 # FONCTIONS D'AFFICHAGE DE GRAPHS
 # ---------------------------------------------------------------------------------
 
 
-def plot_top_x_bar(df_input, colname_name, colvalue_name, top=10):
+def get_top_feats(df_input, colvalue_name, top=10) :
     df_sorted = df_input
     df_sorted['abs_val'] = np.abs(df_sorted[colvalue_name])
     df_sorted = df_input.sort_values(by=['abs_val'], ascending=False).head(top)
+    return df_sorted
+
+
+def plot_top_x_bar(df_input, colname_name, colvalue_name, top=10):
+    df_sorted = get_top_feats(df_input= df_input, colvalue_name=colvalue_name, top=top)
     # Reinverting for display purposes
     df_sorted = df_sorted.sort_values(by=['abs_val'], ascending=True)
-    fig = px.bar(df_sorted, x=colvalue_name, y=colname_name, orientation='h')
+    ## here I'm adding a column with colors
+    df_sorted.loc[df_sorted[colvalue_name]<=0, 'color'] = 'green'
+    df_sorted.loc[df_sorted[colvalue_name]>0, 'color'] = 'red'
+    fig = go.Figure()
+    fig.add_trace(
+    go.Bar(name='Net',
+           x=df_sorted[colvalue_name],
+           y=df_sorted[colname_name],
+           marker_color=df_sorted['color'],
+           orientation='h'))
+    fig.update_layout(barmode='stack')
+    # fig.show()
+    # fig = px.bar(df_sorted, x=colvalue_name, y=colname_name, orientation='h')
 
-    # Resize text
-    fig.update_layout(
-        font=dict(
-            family="Courier New, monospace",
-            size=18,  # Set the font size here
-            color="RebeccaPurple"
-        )
-    )
+    # # Resize text
+    # fig.update_layout(
+    #     font=dict(
+    #         family="Courier New, monospace",
+    #         size=18,  # Set the font size here
+    #         color=df_sorted['color'].values.tolist()
+    #     )
+    # )
     return fig
 
 
@@ -155,6 +189,24 @@ def visualize_radar_chart(data):
     st.pyplot(fig)
     return
 
+
+def gauge_plot(value) :
+    couleur = 'green'
+    if value>50 :
+        couleur='red'
+    fig = go.Figure(go.Indicator(
+        mode = "gauge+number",
+        value = value,
+        gauge = {'axis': {'visible': False, 
+                          'range' : [0, 100]},
+                 'bar': {'color': couleur}},
+        domain = {'x': [0, 1], 'y': [0, 1]}
+        # title = {'text': "Probabilite d'etre en defaut\n",
+        #          'font': {'size': 24}}
+        )
+        )
+
+    return fig
 
 # ---------------------------------------------------------------------------------
 # FONCTIONS AFFICHAGE PAGE WEB
@@ -204,27 +256,56 @@ def main():
                 if predict_btn:
                     if sk_id_combo is not None:
 
+                        # AFFICHAGE PREDICTION
+                        # Input data display au cas ou
+                        # st.write({k: v for k, v in zip(liste_colonnes, data_client.tolist()[0])})
+                        predict_proba = get_predict_proba(lst_columns=liste_colonnes,
+                                                             associated_data=data_client.tolist()[0])
+                        # CSI_ : C'est un peu sale, mais c'est pour finir dans les temps
+                        proba_1 = predict_proba.split(' ')[-1].replace(']', '').replace('"', '')
+                        float_probs = float(proba_1)
 
                         # AFFICHAGE PREDICTION
                         prediction = predict_with_heroku_app(lst_columns=liste_colonnes,
                                                              associated_data=data_client.tolist()[0])
 
+                        client = str(sk_id_combo)
                         if prediction == '0':
-                            text_result = "<span class='ok_customer'> OK </span>"
-                            st.markdown(text_result, unsafe_allow_html=True)
+                            text_result = "<span class='ok_customer'> rembourse </span>"
+                            contexte_proba = (1-float_probs)
+                            # st.markdown(text_result, unsafe_allow_html=True)
                         else:
-                            text_result = "<span class='nok_customer'> NOT OK </span>"
-                            st.markdown(text_result, unsafe_allow_html=True)
+                            text_result = "<span class='nok_customer'> ne rembourse pas </span>"
+                            contexte_proba = float_probs
+                            # st.markdown(text_result, unsafe_allow_html=True)
+
+                        sentence = f"<span class='text_result'>Selon le modele, il y a <b>{np.round(contexte_proba*100, 1)}%</b> de chance que le client {client} {text_result} son pret.</span><br/><br/>"
+                        st.markdown(sentence, unsafe_allow_html=True)
+                        st.markdown(WARNING_MESSAGE, unsafe_allow_html=True)
+
+
+                        # # Plot gauge
+                        # fig = gauge_plot(float_probs*100)
+                        # st.write(fig)
 
                         # AFFICHAGE SHAP FORCE
-                        st.write("TOP 15 Criteria")
+                        # st.write("TOP 15 Criteria")
                         shap_force = get_shap_force(lst_columns=liste_colonnes,
                                                     associated_data=data_client.tolist()[0])
 
                         df_shape_force = dict_to_pd(shap_force)
 
+                        small_df_sorted = get_top_feats(df_shape_force, colvalue_name='values')
+
                         fig = plot_top_x_bar(df_shape_force, colname_name='features', colvalue_name='values')
                         st.write(fig)
+
+                        points_forts = small_df_sorted[small_df_sorted['values'] <0].sort_values(by=['values'], ascending=True)['features'].values.tolist()
+                        points_faibles =  small_df_sorted[small_df_sorted['values'] >=0]['features'].values.tolist()
+                        str_point_forts = "<span class='text_result'>Les <b>points forts</b> de ce dossier sont : "+create_html_list(points_forts[:3])+"</span>"
+                        str_point_faibles = "<span class='text_result'>Les <b>points faibles</b> de ce dossier sont : "+create_html_list(points_faibles[:3])+"</span>"
+                        st.markdown(str_point_forts, unsafe_allow_html=True)
+                        st.markdown(str_point_faibles, unsafe_allow_html=True)
                     else:
                         st.markdown(
                             "ID client manquant.")
--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 # Evaluer des demandes de prets.
 
-## Boites a outils
-- outils_feature_engineering_810.py :
-- outils_grid_search_810.py :
-- outils_classification_810.py : 
-- outils_mlflow_810.py :
+Attention : Pour pouvoir exécuter les codes présents dans ce repo, il est nécessaire de créer d'abord un dossier "input_data" dans lequel déposer les fichiers .csv du projet Kaggle. Ces fichiers étant très volumineux, il nous était impossible de les référencer dans GitHub ou de les déposer sur la plateforme OC.
 
-## Preprocessing des donnes custom
+
+## Description générale des fichiers et dossiers
+### Boites a outils
+- outils_feature_engineering_810.py : des fonctions utiles au feature engineering (analyses de corrélations, encodage des features catégorielles, étude des valeurs manquantes, etc.)
+- outils_grid_search_810.py : des fonctions dédiées à l'analyse de résultats des grid search
+- outils_classification_810.py : des fonctions dédiées à l'analyse de modèles de classification
+- outils_mlflow_810.py : des fonctions dédiées à l'enregistrement d'informations dans ML-Flow
+
+### Preprocessing des donnes custom
 - mbr_kernel.py : Ensemble de fonctions de preprocessing des donnees, i.e feature engineering, cleaning, etc.
 
-## Analyse et modelisations
+### Analyse et modelisations
 
+### Visualisations clients
+- 6_dashboard.py
 
-## Visualisations clients
-- 5_dashboard.py
-
-
-## Test 
+### Test divers
 Tous les fichiers commencant par "wip_"
 
 
